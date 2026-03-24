a21325ff3d47d9b1c340144d0cb488154dd8da75
CAREXM\kamal.lohani <kamal.lohani@carexm.com>
2026-03-24 23:12:07 +0530

    Fix DbContext name for migration

--- backend/AuthService/Program.cs
+++ backend/AuthService/Program.cs
@@ -1,12 +1,13 @@
 using AuthService.Data;
 using AuthService.Services;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DbContext
 builder.Services.AddDbContext<AuthDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Register services
 builder.Services.AddScoped<PasswordService>();
@@ -40,5 +41,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
