983b0331f887522bfd18d1fa7aee6d3783fd8121
CAREXM\kamal.lohani <kamal.lohani@carexm.com>
2026-04-07 13:05:07 +0530

    Image change in DB

---
+++ backend/AmCart.Services.ProductAPI/Migrations/20260407073500_AddProductImage1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmCart.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImage1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Products",
+                newName: "ProductImage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductImage",
+                table: "Products",
+                newName: "ImageUrl");
+        }
+    }
+}
