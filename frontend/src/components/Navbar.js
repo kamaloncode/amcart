6227937b2e1accd425a752b71dfb2fe7dd91be3c
CAREXM\kamal.lohani <kamal.lohani@carexm.com>
2026-04-08 21:54:00 +0530

    Fix name of app

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = ({ onSearch }) => {
       <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/products")}>
         AmCart
       </h2>
-      <h4>Deployment ID: dcac6136-a03e-4504-800b-99709d28b2cadd </h4>
+      <h4>Deployment ID: dcac6136-a03e-4504-800b-99709d28b2ca </h4>
 
       <input
         type="text"
